4e76b19207d0edabeffadbc7386494508f006564
Danny Spencer <dspencer@oberlin.edu>
2010-12-28 14:17:07 -0500

    Ok, basics exist.

--- app/controllers/webtunes_controller.rb
+++ app/controllers/webtunes_controller.rb
@@ -1,5 +1,12 @@
+require 'applescript'
 class WebtunesController < ApplicationController
   def interface
+    puts "Hello, world."
+    AppleScript.execute('tell application "iTunes"
+      set sound volume to 40
+      play
+    end tell')
+    puts "goodbye, world."
   end
 
   #skip the current_song
