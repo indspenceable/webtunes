954c7b8db510f7c649c572bf3139941836ab7c8c
Danny Spencer <dspencer@oberlin.edu>
2010-12-28 13:07:02 -0500

    First commit. Just standard rails stuff.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webtunes::Application.config.secret_token = 'f2b0e05237007ef39166b5da9bf1a648586abde89211d33634de94374a0a76a1dddf4fa90160330db21cd321e477c20550f62203001850c8aaa2348971b5dda2'
