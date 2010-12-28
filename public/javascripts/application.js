84ad5ee507d6de920a8e66c9bb25df2177d3fb9a
Dane Pieri <dpieri@macintosh-375.local>
2010-12-28 16:35:45 -0500

    play pause button switching

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,13 @@
+var is_paused = true;
 $(document).ready(function() {
     $('#play_pause').click(function() {
-      $.post('play_pause')
+      $.post('play_pause');
+			is_paused = !is_paused;
+			if (is_paused){
+				$('#play_pause').css('background', "url('/images/play.png')");
+			} else {
+				$('#play_pause').css('background', "url('/images/pause.png')");
+			}
       });
     $('#next').click(function() {
       $.post('next')
@@ -11,4 +18,4 @@ $(document).ready(function() {
     $('#vol').click(function() {
       $.post('set_volume',{level: 40})
       });
-    })
+})
