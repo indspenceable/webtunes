915c7b21af0a6145f387e3ccbe4296a7c10591a9
Dane Pieri <dpieri@macintosh-375.local>
2010-12-28 22:31:58 -0500

    Finished drag and drop

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,7 @@
-var is_paused = true;  //Need to set this from iTunes current state
 $(document).ready(function() {
     $('#play_pause').click(function() {
       $.post('play_pause');
-			is_paused = !is_paused;
-			if (is_paused){
-				$('#play_pause').css('background', "url('/images/play.png')");
-			} else {
-				$('#play_pause').css('background', "url('/images/pause.png')");
-			}
+			$('#play_pause').toggleClass( 'pause')
       });
     $('#next').click(function() {
       $.post('next')
