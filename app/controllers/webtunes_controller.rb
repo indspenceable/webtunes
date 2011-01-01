39db8013c3fdae414f01dc9e902075febef54e05
Danny Spencer <dspencer@oberlin.edu>
2011-01-01 18:39:54 -0500

    Made the status updater actually work.

--- app/controllers/webtunes_controller.rb
+++ app/controllers/webtunes_controller.rb
@@ -1,4 +1,5 @@
-require 'applescript'
+class AppleScriptError < StandardError; end
+
 class WebtunesController < ApplicationController
   def interface
   end
@@ -9,14 +10,17 @@ class WebtunesController < ApplicationController
   #toggle playing status
   def play_pause
     itunes 'playpause'
+    render :json => get_itunes_status
   end
   #skip the current_song
   def next
     itunes 'next track'
+    render :json => get_itunes_status
   end
   # back to the previous track
   def back
     itunes 'back track'
+    render :json => get_itunes_status
   end
   def set_volume
     itunes "set sound volume to #{params[:level]}"
@@ -26,13 +30,59 @@ class WebtunesController < ApplicationController
   private
   def itunes action
     #vim coloring gets messed up if I use %s{. Sorry.
-    AppleScript.execute("tell application \"iTunes\" 
-                          #{action} 
-                        end tell")
+    as_execute("tell application \"iTunes\" 
+                  #{action} 
+                end tell")
+
   end
 
+  def as_execute(script)
+    #from applescript gem, by Lucas Carlson
+    osascript = `which osascript`
+    if not osascript.empty? and File.executable?(osascript)
+      raise AppleScriptError, "osascript not found, make sure it is in the path"
+    else
+      #result = `osascript -e "#{script.gsub('"', '\"')}" 2>&1`
+      result = `osascript -s s -e "#{script.gsub('"', '\"')}"`
+      if result =~ /execution error/
+        raise AppleScriptError, result
+      end
+      result
+    end
+  end
+
+  #TODO - this doesn't HALT on poorly formed input.
+  #this takes a list returned by 
+  def split_by_strings s
+    c = 2 # SHOULD BE A {"
+    rtn = []
+    while c < s.size - 2
+      start = c
+      while s[c] != "\"" && s[c-1] != "\\"
+        c += 1
+      end
+      rtn << "#{s[start, c-start]}"
+      c += 4
+    end
+    rtn
+  end
+  def split_by_numbers s
+    c = 1 # SHOULD BE A {
+    rtn = []
+    while c < s.size - 1
+      start = c
+      while s[c] && c < s.size-1 != ","
+        c += 1
+      end
+      rtn << "#{s[start, c-start]}"
+      c += 2
+    end
+    rtn
+  end
   def get_itunes_status
-    'get sound volume'
+    names = itunes "get name of every track of current playlist"
+    artists = itunes "get artist of every track of current playlist"
+    durations = itunes "get duration of every track of current playlist"
+    [(split_by_strings names), (split_by_strings artists), (split_by_numbers durations)]
   end
-
 end
